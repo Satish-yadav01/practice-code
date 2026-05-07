2f899ab66074f767ee9c385db68ddc820c3e88ef
satishyadav <satishyadav.ssy02@gmail.com>
2026-05-08 03:55:44 +0530

    Singleton practice

--- src/designpattern/creational/singleton/Driver.java
+++ src/designpattern/creational/singleton/Driver.java
@@ -2,18 +2,42 @@ package designpattern.creational.singleton;
 
 public class Driver {
     public static void main(String[] args) {
-        InMemoryCache memoryCache = InMemoryCache.getInstance();
-        memoryCache.put("satish","1");
-        memoryCache.put("Abhishek","2");
-        memoryCache.put("jayesh","3");
-        memoryCache.put("chandu","4");
+//        InMemoryCache memoryCache = InMemoryCache.getInstance();
+//        memoryCache.put("satish","1");
+//        memoryCache.put("Abhishek","2");
+//        memoryCache.put("jayesh","3");
+//        memoryCache.put("chandu","4");
+//
+//        System.out.println(memoryCache.get("jayesh"));
+//
+//        InMemoryCache memoryCache1 = InMemoryCache.getInstance();
+//        System.out.println(memoryCache1.get("jayesh"));
+//
+//        InMemoryCache memoryCache2 = InMemoryCache.getInstance();
+//        System.out.println(memoryCache2.get("jayesh"));
 
-        System.out.println(memoryCache.get("jayesh"));
+        //test the singleton with multiple threads
+//        for (int i = 0; i < 10; i++) {
+//            Thread thread = new Thread(() -> {
+//                InMemoryCache cache = InMemoryCache.getInstance();
+//                cache.print();
+//            }, "Thread-" + i);
+//            thread.start();
+//        }
 
-        InMemoryCache memoryCache1 = InMemoryCache.getInstance();
-        System.out.println(memoryCache1.get("jayesh"));
+        //test the concurrency of cache whether that will update properly or not.
+        // need a sequential update 10 - 20 - 30 - 40 - 50
+        System.out.println("Map Testing");
+        InMemoryCache instance = InMemoryCache.getInstance();
+            for (int i = 0; i < 5; i++) {
+                int finalI = i;
+                Thread thread = new Thread(() -> instance.put("A", finalI *10),"Thread-"+i);
+                thread.start();
+            }
 
-        InMemoryCache memoryCache2 = InMemoryCache.getInstance();
-        System.out.println(memoryCache2.get("jayesh"));
+
+        instance.print();
+
+
     }
 }
--- src/designpattern/creational/singleton/InMemoryCache.java
+++ src/designpattern/creational/singleton/InMemoryCache.java
@@ -2,28 +2,41 @@ package designpattern.creational.singleton;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class InMemoryCache {
-    private Map<String , String> cache = new HashMap<>();
+    private final Map<String,Integer> cache = new ConcurrentHashMap<>();
     private static InMemoryCache inMemoryCache;
-    private InMemoryCache() {}
+    private InMemoryCache() {
+        System.out.println("Instance created by: " + Thread.currentThread().getName());
+    }
 
-    public static synchronized InMemoryCache getInstance(){
+    public static InMemoryCache getInstance(){
         if (inMemoryCache == null) {
-            inMemoryCache =  new InMemoryCache();
+            synchronized (InMemoryCache.class){
+                if(inMemoryCache == null){
+                    inMemoryCache =  new InMemoryCache();
+                }
+            }
         }
         return inMemoryCache;
     }
 
-    public void put(String key, String value) {
-        cache.putIfAbsent(key, value);
+    public void put(String key, int value) {
+        if(cache.containsKey(key))  System.out.println("key : " + key + ",value : " + value);
+        cache.put(key, value);
     }
 
-    public String get(String key) {
+    public int get(String key) {
         return cache.get(key);
     }
 
-    public Map<String , String> getAll() {
+    public Map<String , Integer> getAll() {
         return cache;
     }
+
+    public void print(){
+        System.out.println("printing by: " + Thread.currentThread().getName() + ", cache data : " + cache);
+    }
+
 }
